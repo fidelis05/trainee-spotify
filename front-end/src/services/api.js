dffcacdeaedf7247ab46fb5772ca7567b98e342c
fidelis05 <100060822+fidelis05@users.noreply.github.com>
2025-02-21 01:04:17 -0300

    VERCEL DEPLOY

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://trainee-spotify.vercel.app/api",
+    baseURL: "http://trainee-spotify-api.vercel.app/api",
     withCredentials: true,
 });
 
